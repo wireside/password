5d263a8460af1b0fd35b4349fb619e4ed9bd80bc
wireside <wiresifed@gmail.com>
2025-04-27 22:52:21 +0300

    created pointer to print address in memory to console

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
+	a := 5
+	var pointerA *int = &a // "*" before type name means pointer to the variable with that type
+	// pointerA := &a
 	
+	fmt.Println(pointerA) // prints the address in memory for "a" variable
+}
+
+func double(pointer *int) {
+
 }
