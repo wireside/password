23a0880df7b12d941200cbb7109aa8b7f4e087be
wireside <wiresifed@gmail.com>
2025-04-28 00:20:25 +0300

    feat: created 'double' function that can change the value of variable using pointer on variable

--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import "fmt"
 
 func main() {
 	a := 5
-	var pointerA *int = &a // "*" before type name means pointer to the variable with that type
+	var pointerA *int = &a // "*" before type abbreviation means pointer to the variable with that type
 	// pointerA := &a
+	fmt.Println(pointerA)  // prints the address in memory for "a" variable
+	fmt.Println(*pointerA) // * means dereference for pointer,
+	// in other words it's way to get the variable and its value from pointer
 	
-	fmt.Println(pointerA) // prints the address in memory for "a" variable
+	fmt.Println("Value before:", a)
+	
+	double(&a)
+	
+	fmt.Println("Value after:", a)
+	
+	// with a pointer you can pass the variable as reference, not as value
+	// reference types like slice, map, function, chanel passes as reference by default
 }
 
-func double(pointer *int) {
-
+func double(num *int) {
+	*num = *num * 2
 }
