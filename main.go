c42d5425b897887f271b15cd021381c019476900
wireside <wiresifed@gmail.com>
2025-04-28 20:19:39 +0300

    refactor: use derefernce (*arr)[] to change the value by index

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func reverse(arr *[4]int) {
 	// without dereference we get current value of array on every iteration
 	// because arr is pointer(reference), but *arr is a value that range uses once
 	for index, value := range *arr {
-		arr[len(arr) - 1 - index] = value
+		(*arr)[len(arr) - 1 - index] = value
 	}
 }
 
