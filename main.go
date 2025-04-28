d4720cb3af0dabaaddbdc30af4c44f6681e45da6
wireside <wiresifed@gmail.com>
2025-04-28 23:16:46 +0300

    created account struct and its instances

--- main.go
+++ main.go
@@ -2,20 +2,45 @@ package main
 
 import "fmt"
 
+type account struct {
+	login    string
+	password string
+	url      string
+}
+
 func main() {
-	a := [4]int{1, 2, 3, 4}
-	reverse(&a)
-	fmt.Println(a)
-}
-
-func reverse(arr *[4]int) {
-	// without dereference we get current value of array on every iteration
-	// because arr is pointer(reference), but *arr is a value that range uses once
-	for index, value := range *arr {
-		(*arr)[len(arr) - 1 - index] = value
+	login := promptData("Введите логин: ")
+	password := promptData("Введите пароль: ")
+	url := promptData("Введите url: ")
+	
+	account1 := account{
+		login,       // ordering is important
+		password, // passing the parameters in same order as in struct
+		url,
 	}
+	
+	account2 := account{
+		password: password, // property name is important
+		login:    login,    // you can pass the parameters in any order you want
+		url:      url,
+	}
+	
+	account3 := account{
+		password: password, // without login --> login sets to empty string ""
+		url:      url,
+	}
+	
+	emptyAccount := account{} // empty instance which includes default null values
+	
+	fmt.Println(account1, account2, account3, emptyAccount)
 }
 
-func double(num *int) {
-	*num = *num * 2
+func promptData(prompt string) string {
+	fmt.Print(prompt)
+	var res string
+	_, err := fmt.Scan(&res)
+	if err != nil {
+		return ""
+	}
+	return res
 }
