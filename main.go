2300bc0071cf65a8d4d90e7f4bf6c310a7a7e6b1
wireside <wiresifed@gmail.com>
2025-04-29 23:54:56 +0300

    created function that outputs the account's data and optimized it

--- main.go
+++ main.go
@@ -13,26 +13,18 @@ func main() {
 	password := promptData("Введите пароль: ")
 	url := promptData("Введите url: ")
 	
-	account1 := account{
-		login,       // ordering is important
+	myAccount := account{
+		login,    // ordering is important
 		password, // passing the parameters in same order as in struct
 		url,
 	}
 	
-	account2 := account{
-		password: password, // property name is important
-		login:    login,    // you can pass the parameters in any order you want
-		url:      url,
-	}
-	
-	account3 := account{
-		password: password, // without login --> login sets to empty string ""
-		url:      url,
-	}
-	
-	emptyAccount := account{} // empty instance which includes default null values
-	
-	fmt.Println(account1, account2, account3, emptyAccount)
+	outputPassword(&myAccount)
+}
+
+func outputPassword(acc *account) {
+	fmt.Println(acc.login, acc.password, acc.url)  // (*acc).login is similar as acc.login
+	// fmt.Println((*acc).login, (*acc).password, (*acc).url) without shorthand
 }
 
 func promptData(prompt string) string {
