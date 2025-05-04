33d866b76e30b7d81dd4d9a17799d5ae7c8fad82
wireside <wiresifed@gmail.com>
2025-05-04 20:06:47 +0300

    modified project structure to multi-package application

---
+++ account/account.go
@@ -0,0 +1,77 @@
+package account
+
+import (
+	"errors"
+	"fmt"
+	"math/rand/v2"
+	"net/url"
+	"time"
+)
+
+var availableLetterRunes = []rune(
+	"abcdefghijklmnopqrstuvwxyz" +
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-*@!_.",
+)
+
+var (
+	invalidUrlError   = errors.New("url is invalid")
+	invalidLoginError = errors.New("login is empty")
+)
+
+type Account struct {
+	login    string
+	password string
+	url      string
+}
+
+type AccountWithTimeStamp struct {
+	createdAt time.Time
+	updatedAt time.Time
+	Account
+}
+
+func (acc *Account) OutputPassword() {
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *Account) generatePassword(n int) {
+	res := make([]rune, n)
+
+	for i := range res {
+		res[i] = availableLetterRunes[rand.IntN(len(availableLetterRunes)-1)]
+	}
+
+	acc.password = string(res)
+}
+
+func NewAccountWithTimeStamp(login, password, urlString string) (
+	*AccountWithTimeStamp, error,
+) {
+	if login == "" {
+		return nil, invalidLoginError
+	}
+
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, invalidUrlError
+	}
+
+	newAcc := AccountWithTimeStamp{
+		createdAt: time.Now(),
+		updatedAt: time.Now(),
+		Account: Account{
+			login:    login,
+			password: password,
+			url:      urlString,
+		},
+	}
+
+	if password == "" {
+		// shorthand can be used only if embedded struct was declared by short way
+		newAcc.generatePassword(12) // is similar to
+		// newAcc.Account.generatePassword(12)
+		// newAcc.login = no_password_user
+	}
+
+	return &newAcc, nil
+}
--- main.go
+++ main.go
@@ -1,111 +1,21 @@
 package main
 
 import (
-	"errors"
+	"demo/password/account"
 	"fmt"
-	"math/rand/v2"
-	"net/url"
-	"time"
 )
-
-var availableLetterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-*@!_.")
-
-var (
-	InvalidUrlError   = errors.New("url is invalid")
-	InvalidLoginError = errors.New("login is empty")
-)
-
-type account struct {
-	login    string
-	password string
-	url      string
-}
-
-type accountWithTimeStamp struct { // composition struct
-	createdAt time.Time
-	updatedAt time.Time
-	account // simple and preferred way
-	// acc  account --> second and longer way to use composition
-}
-
-func (acc *account) outputPassword() { // (acc account) is a copy of struct instance
-	fmt.Println(acc.login, acc.password, acc.url)
-}
-
-func (acc *account) generatePassword(n int) { // acc *account is a pointer to instance
-	res := make([]rune, n)
-
-	for i := range res {
-		res[i] = availableLetterRunes[rand.IntN(len(availableLetterRunes)-1)]
-	}
-
-	acc.password = string(res)
-}
-
-func newAccountWithTimeStamp(login, password, urlString string) (*accountWithTimeStamp, error) {
-	if login == "" {
-		return nil, InvalidLoginError
-	}
-	
-	_, err := url.ParseRequestURI(urlString)
-	if err != nil {
-		return nil, InvalidUrlError
-	}
-	
-	newAcc := accountWithTimeStamp{
-		createdAt: time.Now(),
-		updatedAt: time.Now(),
-		account: account{
-			login:    login,
-			password: password,
-			url:      urlString,
-		},
-	}
-	
-	if password == "" {
-		// shorthand can be used only if embedded struct was declared by short way
-		newAcc.generatePassword(12) // is similar to
-		// newAcc.account.generatePassword(12)
-		// newAcc.login = no_password_user
-	}
-	
-	return &newAcc, nil
-}
-
-func newAccount(login, password, urlString string) (*account, error) { // account's constructor
-	if login == "" {
-		return nil, InvalidLoginError
-	}
-
-	_, err := url.ParseRequestURI(urlString)
-	if err != nil {
-		return nil, InvalidUrlError
-	}
-
-	newAcc := account{
-		login:    login,
-		password: password,
-		url:      urlString,
-	}
-
-	if password == "" {
-		newAcc.generatePassword(12)
-	}
-
-	return &newAcc, nil
-}
 
 func main() {
 	login := promptData("Введите логин: ")
 	password := promptData("Введите пароль: ")
 	urlString := promptData("Введите url: ")
 
-	myAccount, err := newAccountWithTimeStamp(login, password, urlString)
+	myAccount, err := account.NewAccountWithTimeStamp(login, password, urlString)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	myAccount.outputPassword()
+	myAccount.OutputPassword()
 	fmt.Println(myAccount)
 }
 
