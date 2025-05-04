77055f5522603f680c6586480aa8a71c46f1c376
wireside <wiresifed@gmail.com>
2025-05-04 17:41:18 +0300

    created new struct based on account struct using composition

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"math/rand/v2"
 	"net/url"
+	"time"
 )
 
 var availableLetterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-*@!_.")
 
 var (
-	InvalidUrlError = errors.New("url is invalid")
+	InvalidUrlError   = errors.New("url is invalid")
 	InvalidLoginError = errors.New("login is empty")
 )
 
@@ -18,6 +19,13 @@ type account struct {
 	login    string
 	password string
 	url      string
+}
+
+type accountWithTimeStamp struct { // composition struct
+	createdAt time.Time
+	updatedAt time.Time
+	account // simple and preferred way
+	// acc  account --> second and longer way to use composition
 }
 
 func (acc *account) outputPassword() { // (acc account) is a copy of struct instance
@@ -34,7 +42,7 @@ func (acc *account) generatePassword(n int) { // acc *account is a pointer to in
 	acc.password = string(res)
 }
 
-func newAccount(login, password, urlString string) (*account, error) {
+func newAccountWithTimeStamp(login, password, urlString string) (*accountWithTimeStamp, error) {
 	if login == "" {
 		return nil, InvalidLoginError
 	}
@@ -43,13 +51,43 @@ func newAccount(login, password, urlString string) (*account, error) {
 	if err != nil {
 		return nil, InvalidUrlError
 	}
-
-	newAcc := account{
-		login: login,
-		password: password,
-		url:   urlString,
+	
+	newAcc := accountWithTimeStamp{
+		createdAt: time.Now(),
+		updatedAt: time.Now(),
+		account: account{
+			login:    login,
+			password: password,
+			url:      urlString,
+		},
 	}
 	
+	if password == "" {
+		// shorthand can be used only if embedded struct was declared by short way
+		newAcc.generatePassword(12) // is similar to
+		// newAcc.account.generatePassword(12)
+		// newAcc.login = no_password_user
+	}
+	
+	return &newAcc, nil
+}
+
+func newAccount(login, password, urlString string) (*account, error) { // account's constructor
+	if login == "" {
+		return nil, InvalidLoginError
+	}
+
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, InvalidUrlError
+	}
+
+	newAcc := account{
+		login:    login,
+		password: password,
+		url:      urlString,
+	}
+
 	if password == "" {
 		newAcc.generatePassword(12)
 	}
@@ -62,12 +100,13 @@ func main() {
 	password := promptData("Введите пароль: ")
 	urlString := promptData("Введите url: ")
 
-	myAccount, err := newAccount(login, password, urlString)
+	myAccount, err := newAccountWithTimeStamp(login, password, urlString)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	myAccount.outputPassword()
+	fmt.Println(myAccount)
 }
 
 func promptData(prompt string) string {
